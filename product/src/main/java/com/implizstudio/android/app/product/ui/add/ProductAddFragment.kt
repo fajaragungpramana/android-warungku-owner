251e6d475c7f5ad6dc62b12419314dd409b55233
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-01-19 10:43:12 +0700

    Adding date picker dialog when form field expired is clicked

--- product/src/main/java/com/implizstudio/android/app/product/ui/add/ProductAddFragment.kt
+++ product/src/main/java/com/implizstudio/android/app/product/ui/add/ProductAddFragment.kt
@@ -1,5 +1,6 @@
 package com.implizstudio.android.app.product.ui.add
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,8 +9,22 @@ import com.implizstudio.android.app.components.field.UnitField
 import com.implizstudio.android.app.product.R
 import com.implizstudio.android.app.product.databinding.FragmentAddProductBinding
 import com.implizstudio.android.app.resources.base.FragmentBase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProductAddFragment : FragmentBase<FragmentAddProductBinding>() {
+
+    private val calendar = Calendar.getInstance()
+    private val date = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+        calendar.apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month)
+            set(Calendar.DAY_OF_MONTH, dayOfMonth)
+        }
+        getBinding()?.tieAddExpired?.setText(
+            SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(calendar.time)
+        )
+    }
 
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentAddProductBinding.inflate(inflater, container, false)
@@ -32,6 +47,16 @@ class ProductAddFragment : FragmentBase<FragmentAddProductBinding>() {
                 }
             }
             ufAddMinOrder.setUnitText(availableList[0])
+
+            tieAddExpired.setOnClickListener {
+                DatePickerDialog(
+                    requireActivity(),
+                    date,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
 
         }
 
