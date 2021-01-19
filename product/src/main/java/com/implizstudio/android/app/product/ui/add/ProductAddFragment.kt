3e57dfa81b534496538d80c9fc39173296347f40
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-01-19 10:25:49 +0700

    Initialize form field

--- product/src/main/java/com/implizstudio/android/app/product/ui/add/ProductAddFragment.kt
+++ product/src/main/java/com/implizstudio/android/app/product/ui/add/ProductAddFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.implizstudio.android.app.components.field.UnitField
+import com.implizstudio.android.app.product.R
 import com.implizstudio.android.app.product.databinding.FragmentAddProductBinding
 import com.implizstudio.android.app.resources.base.FragmentBase
 
@@ -13,10 +15,23 @@ class ProductAddFragment : FragmentBase<FragmentAddProductBinding>() {
         FragmentAddProductBinding.inflate(inflater, container, false)
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        val availableList = resources.getStringArray(R.array.available_unit)
 
         getBinding()?.apply {
 
             ivAddIconBack.setOnClickListener { findNavController().navigateUp() }
+
+            ufAddWeight.setUnitList(resources.getStringArray(R.array.weight_unit))
+
+            ufAddAvailable.let {
+                it.setUnitList(availableList)
+                it.unitListener = object : UnitField.UnitListener {
+                    override fun getUnitText(value: String) {
+                        ufAddMinOrder.setUnitText(value)
+                    }
+                }
+            }
+            ufAddMinOrder.setUnitText(availableList[0])
 
         }
 
